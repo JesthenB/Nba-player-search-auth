4d996c54fa8f958d4847ef5dc07342e152e800b3
JesthenB <jesthenh@gmail.com>
2024-02-07 01:58:09 -0600

    added search box and button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,12 @@
 export default function Dashboard() {
   return (
     <div className="container">
+      <form className="search-input">
+        <input type="search"></input>
+        <button type="button">Search</button>
+      </form>
       <div className="card start-hero">
-        <p className="text-body-2 start-hero-intro">Hey Welcome in!</p>
+        <p className="text-body-2 start-hero-intro"></p>
         <p className="text-display-2"></p>
       </div>
     </div>
