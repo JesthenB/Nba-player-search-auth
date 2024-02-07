0045c18d8faf879354b2ad8219c116ade1ce68e0
JesthenB <jesthenh@gmail.com>
2024-02-07 00:10:06 -0600

    background image added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,12 +2,8 @@ export default function Dashboard() {
   return (
     <div className="container">
       <div className="card start-hero">
-        <p className="text-body-2 start-hero-intro">Woohoo!</p>
-        <p className="text-display-2">
-          Your authentication is all sorted.
-          <br />
-          Build the important stuff.
-        </p>
+        <p className="text-body-2 start-hero-intro">Hey Welcome in!</p>
+        <p className="text-display-2"></p>
       </div>
       <section className="next-steps-section">
         <h2 className="text-heading-1">Next steps for you</h2>
