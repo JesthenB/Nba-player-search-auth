d77f1ac4f969258dbbd0478f80f5a73b1f2c66a4
JesthenB <jesthenh@gmail.com>
2024-02-24 14:57:28 -0600

    created fetch method to communicate with external api

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,22 @@
+'use client'
 export default function Dashboard() {
+  const handleClick = async () =>{
+    // console.log('it works');
+    try{
+      const response = await fetch('https://www.thesportsdb.com/api/v1/json/3/searchplayers.php?p=');
+      const data = await response.json();
+      console.log('data from api', data);
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
+};
+    
+
   return (
     <div className="container">
       <form className="search-input">
         <input type="search"></input>
-        <button type="button">Search</button>
+        <button type="button" onClick={handleClick}>Search</button>
       </form>
       <div className="card start-hero">
         <p className="text-body-2 start-hero-intro"></p>
